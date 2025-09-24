dcd117971a71d2c66b977b757fff819262e70f9f
Hassan Ali <haxxanali512@gmail.com>
2025-09-24 19:46:57 +0500

    Fix Sidekiq Redis pool size - increase from 1 to 5

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = {
     url: "redis://hbs_data_processing-redis:6379/0",
-    size: 1
+    size: 5
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: "redis://hbs_data_processing-redis:6379/0",
-    size: 1
+    size: 5
   }
 end
