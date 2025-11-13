7a4b085bca47433e75bc6eaab7a4f28045b78eab
Hassan Ali <haxxanali512@gmail.com>
2025-11-13 13:39:24 +0500

    Updating gemfile

--- test/models/organization_identifier_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class OrganizationIdentifierTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/organization_setting_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class OrganizationSettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
