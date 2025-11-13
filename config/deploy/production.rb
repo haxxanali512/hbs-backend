6e02bd39193931da8e8a6ac79e10ede69aa2d1f1
Hassan Ali <haxxanali512@gmail.com>
2025-11-13 15:38:06 +0500

    Updating creds file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal:true
 
-server "3.148.151.165", user: "deployer", roles: %w[app web worker]
+server "3.148.151.165", user: "deployer", roles: %w[app web worker db]
 
 set :branch, "main"
 set :stage, :production
