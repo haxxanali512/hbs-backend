5a0d8c1cdb40867d38caf66a0354b7b4b5a358f5
Hassan Ali <haxxanali512@gmail.com>
2025-11-13 15:48:42 +0500

    Updating creds file

--- db/migrate/20251022105439_add_locked_to_organization_fee_schedules.rb
+++
@@ -1,5 +0,0 @@
-class AddLockedToOrganizationFeeSchedules < ActiveRecord::Migration[7.2]
-  def change
-    add_column :organization_fee_schedules, :locked, :boolean, default: false, null: false
-  end
-end
